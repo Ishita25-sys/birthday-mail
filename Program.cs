c10f08f4a3c05c8965a77736eba0613f801e99e5
Ishita25-sys <ishu.252502@gmail.com>
2024-07-26 12:24:52 +0530

    raw css link

--- Program.cs
+++ Program.cs
@@ -73,7 +73,7 @@ namespace birthday_mail
                                 <meta charset='UTF-8'>
                                 <meta name='viewport' content='width=device-width, initial-scale=1.0'>
                                 <title>Birthday Post Template</title>
-                                <link rel='stylesheet' href='style1.css'>
+                                <link rel='stylesheet' href='https://raw.githubusercontent.com/Ishita25-sys/birthday-mail/master/style1.css'>
                                 
                             </head>
                             <body>
