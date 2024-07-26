136524d7ee87b2535cb1a4d87618f4c7255a6b5c
Ishita25-sys <ishu.252502@gmail.com>
2024-07-26 12:19:52 +0530

    css external

--- Program.cs
+++ Program.cs
@@ -73,7 +73,7 @@ namespace birthday_mail
                                 <meta charset='UTF-8'>
                                 <meta name='viewport' content='width=device-width, initial-scale=1.0'>
                                 <title>Birthday Post Template</title>
-                                <link rel='stylesheet' href='style.css'>
+                                <link rel='stylesheet' href='style1.css'>
                                 
                             </head>
                             <body>
